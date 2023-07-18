dd6e0c3906163642e424b00761d620962c69649b
Gautham2980 <gautham4816@gmail.com>
2023-07-18 11:54:54 +0530

    pattern1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+		var n=prompt("Enter Number")
+
+for(let i=1; i<=n; i++)
+		{
+			for(let j=1; j<=i; j++)
+				
+				{
+					document.write(i);
+					document.write("&nbsp");
+				}
+				
+					document.write("<br>")
+			
+			
+			
+			
+			
+			
+			
+		}
